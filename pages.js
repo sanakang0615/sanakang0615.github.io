ff50d1300e77dcf517207781bc092b87a71c6fb7
sanakang0615 <sanakang0615@kaist.ac.kr>
2024-05-30 16:20:06 +0900

    Fix: rename github repoURL

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/sanakang0615/sanakang0615.github.io.git";
 
 ghpages.publish(
   pathname,
